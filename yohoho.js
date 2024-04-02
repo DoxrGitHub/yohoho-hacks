06ee49de9c4dfdf4dce477b4809bfdcf41b691c6
DoxrGitHub <132623420+DoxrGitHub@users.noreply.github.com>
2024-04-02 11:54:01 -0500

    Update yohoho.js

--- yohoho.js
+++ yohoho.js
@@ -8,7 +8,5 @@ localStorage.setItem('totalKills', '696969696969696969696')
 localStorage.setItem('totalWins', '696969696969696969696')
 localStorage.setItem('unlockedSkins', "[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35]")
 localStorage.setItem('unlockedPets', "[1,2,3,4,5,6,7,8,9]")
-localStorage.setItem('playerXP', "696969696969696969696")
-localStorage.setItem('bestScore', "696969696969696969696")
 localStorage.setItem('username', "Injection Sucessful")
 location.reload();
